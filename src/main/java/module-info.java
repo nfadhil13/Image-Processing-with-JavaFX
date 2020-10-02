8f1c7a7f4b54bf98d54dc20bdd616e2abc504a00
nfadhil13 <48423700+nfadhil13@users.noreply.github.com>
2020-10-02 22:45:29 +0700

    Newest

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
-//module org.fdev {
-//    requires javafx.controls;
-//    requires javafx.fxml;
-//    requires opencv;
-//
-//    opens org.fdev.controller to javafx.fxml;
-//    exports org.fdev;
-//
-//
-//}
+module org.fdev {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires opencv;
+
+    opens org.fdev.controller to javafx.fxml;
+    exports org.fdev;
+
+
+}
